8998c38c361fb3b81a22421c2e2f6efcaef55f95
Jorvi Aviles <jorviaviles@gmail.com>
2025-03-11 21:39:46 -0600

    Create hola.php

---
+++ php/hola.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Hola Mundo en PHP</title>
+</head>
+<body>
+    <h1><?php echo "Hola Mundo"; ?></h1>
+</body>
+</html>
