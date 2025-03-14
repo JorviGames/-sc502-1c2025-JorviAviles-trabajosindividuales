05aa7aada4c76ffb915f62285c888efa4f6bc50b
Jorvi Aviles <jorviaviles@gmail.com>
2025-03-13 19:19:15 -0600

    practica

---
+++ semana9/backend/db.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$dbname = "todo_app";
+$user = "Jorvi@localhost";
+$password = "132430jorviA";
+
+try {
+
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "conexion exitosa". PHP_EOL;
+}catch(Exception $e){
+    die("Error de conexion: ". $e -> getMessage());
+}
