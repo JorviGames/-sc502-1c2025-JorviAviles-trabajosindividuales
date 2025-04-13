ec1bfdebcff6aaf1fc72e97cbfd02703a9c9c67b
Jorvi Aviles <jorviaviles@gmail.com>
2025-04-13 16:19:04 -0600

    semana12

--- Semana12/app-tareas/backend/db.php
+++ Semana12/app-tareas/backend/db.php
@@ -9,7 +9,7 @@ try {
 
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   // echo "conexion exitosa". PHP_EOL;
+    echo "conexion exitosa". PHP_EOL;
 }catch(Exception $e){
     die("Error de conexion: ". $e -> getMessage());
 }
